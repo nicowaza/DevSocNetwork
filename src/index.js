9e8c39929570ad310c0e739cb6a821f700b0b0c3
ducteil nicolas <ducteil.n@gmail.com>
2018-07-21 16:32:57 +0200

    VMC model architecture

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 const app = express();
 import 'dotenv/config'
+import bodyparser from 'body-parser'
+import volleyball from 'volleyball'
+import passport from 'passport'
 import mongoose from 'mongoose'
 import {
   connect
 } from './config/db'
 
 connect()
+
+app.use(volleyball)
+app.use(express.json)
+app.use(express.urlencoded({
+  extended: true
+}))
 
 
 app.get("/", (req, res) => res.send('hi'));
